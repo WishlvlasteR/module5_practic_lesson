0d7d5ee26d94d254412a065ed27ae1deb532e759
Ruslan <wishiviaster99@gmail.com>
2024-12-15 20:59:54 +1200

    Add files via upload

--- module5_praktic_lesson_1.py
+++ module5_praktic_lesson_1.py
@@ -10,7 +10,7 @@ class House():
         Выводит этажи от 1 до new_floor, если он существует.
         Если этаж недопустим, выводит сообщение об ошибке.
         """
-        if new_floor <= self.number_of_floors > 1:
+        if self.number_of_floors >= new_floor > 0:
             for i in range(1, new_floor + 1):
                 print(i)
         else:
@@ -19,5 +19,5 @@ class House():
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
-h1.go_to(19)
-h2.go_to(2)
+h1.go_to(18)
+h2.go_to(0)
