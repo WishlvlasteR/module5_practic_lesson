0c1b52d0373ffca81425e79c643eb2f5e5cd8063
Ruslan <wishiviaster99@gmail.com>
2024-12-15 20:44:38 +1200

    Add files via upload

---
+++ module5_practic_lesson_2.py
@@ -0,0 +1,40 @@
+class House():
+    def __init__(self, name, number_of_floors):
+        """Инициализация объекта House с названием и количеством этажей."""
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        """
+        Переход на заданный этаж.
+        Выводит этажи от 1 до new_floor, если он существует.
+        Если этаж недопустим, выводит сообщение об ошибке.
+        """
+        if new_floor <= self.number_of_floors > 1:
+            for i in range(1, new_floor + 1):
+                print(i)
+        else:
+            print("Такого этажа не существует")
+
+    def __len__(self):
+        """Возвращает количество этажей в здании."""
+        return self.number_of_floors
+
+    def __str__(self):
+        """
+        Возвращает строку с названием зданий
+        и количеством этажей в этом здании.
+        """
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
